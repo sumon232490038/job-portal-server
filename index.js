78786ac87de238651914e9920485b2b1d7d725ec
sumon232 <sumonmia4526@gmail.com>
2024-12-26 01:00:13 +0600

    Alhamdulillah jwt is added and working properly

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("bangladesh jindabad");
 });
+
+const verifyToken = (req, res, next) => {
+  const token = req?.cookies?.token;
+  // console.log(token);
+  if (!token) {
+    return res.status(401).send({ message: "Your is a Unauthorized parson" });
+  }
+  jwt.verify(token, process.env.JWT_PASS, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "Error detected in your token" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xhl2h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -30,13 +53,24 @@ async function run() {
       .db("jobPortal")
       .collection("job-application");
 
+    // Auth token by JWT
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.JWT_PASS, { expiresIn: "4h" });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false,
+        })
+        .send({ success: true });
+    });
+
     app.get("/jobs", async (req, res) => {
       const email = req.query.email;
       let query = {};
       if (email) {
         query = { hr_email: email };
       }
-      console.log(email);
       const cursor = jobsCollections.find(query);
       const result = await cursor.toArray();
       res.send(result);
@@ -52,9 +86,16 @@ async function run() {
 
     //Jobs apis
 
-    app.get("/job-applications", async (req, res) => {
+    app.get("/job-applications", verifyToken, async (req, res) => {
       const query = req.query.email;
       const query2 = { Applicat_email: query };
+      console.log(req.user);
+      if (req.user.email !== req.query.email) {
+        return res.status(403).send({
+          message: "Your are not is this info perfect",
+        });
+      }
+      // console.log(req.cookies);
       const result = await jobsApplicationCollections.find(query2).toArray();
       for (const application of result) {
         const query1 = { _id: new ObjectId(application.job_id) };
